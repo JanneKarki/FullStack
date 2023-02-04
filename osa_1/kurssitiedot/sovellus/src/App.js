cbd6093a0382830409e9c32098bb18364b6c4303
Janne Kärki <janne.karki@helsinki.fi>
2023-02-04 10:56:11 +0200

    Refracted content component

--- osa_1/kurssitiedot/sovellus/src/App.js
+++ osa_1/kurssitiedot/sovellus/src/App.js
@@ -7,6 +7,28 @@ const Header = () => {
   )
 }
 
+
+const Content = () => {
+  const parts = [
+    { name:'Fundamentals of react', exercises: 10},
+    { name:'Using props to pass data', exercises: 7},
+    { name:'State of component', exercises: 14}
+  ]
+  return (
+    <div>
+      <p>
+        {parts[0].name} {parts[0].exercises}
+      </p>
+      <p>
+      {parts[1].name} {parts[1].exercises}
+      </p>
+      <p>
+      {parts[2].name} {parts[2].exercises}
+      </p>
+    </div>
+  )
+}
+
 const App = () => {
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
@@ -18,18 +40,11 @@ const App = () => {
   return (
     <div>
       <Header />
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+      <Content />
       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   )
 }
+
 
 export default App
