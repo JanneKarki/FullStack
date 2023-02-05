e304da76bb8d15828f08ec10cf82bafeb9148ea3
Janne Kärki <janne.karki@helsinki.fi>
2023-02-05 10:38:36 +0200

    task 1.6: done

--- osa_1/unicafe/src/App.js
+++ osa_1/unicafe/src/App.js
@@ -1,14 +1,50 @@
 import { useState } from 'react'
 
+const Button = (props) => {
+  return (
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
+  )
+}
+
+const Display = (props) => {
+  return (
+    <div>
+      <b>{props.text} {props.counter}</b>
+    </div>
+  )
+}
+
 const App = () => {
   // tallenna napit omaan tilaansa
+  const header = 'Give feedback'
+  const statistics = "Statistics"
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const increaseGood = () => {
+    setGood(good +1)
+  }
+  const increaseNeutral = () => {
+    setNeutral(neutral +1)
+  }
+  const increaseBad = () => {
+    setBad(bad +1)
+  }
+
   return (
     <div>
-      code here
+      <h1>{header}</h1>
+      <Button handleClick={increaseGood} text="Good" />
+      <Button handleClick={increaseNeutral} text="Neutral" />
+      <Button handleClick={increaseBad} text="Bad" />
+      <h1>{statistics}</h1>
+      <Display counter={good} text="good"/>
+      <Display counter={neutral} text="neutral"/>
+      <Display counter={bad} text="bad"/>
+
     </div>
   )
 }
