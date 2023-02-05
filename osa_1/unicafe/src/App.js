6638bd9bb92bf6306c3dabd442ff4c3d31b1b944
Janne Kärki <janne.karki@helsinki.fi>
2023-02-05 12:22:33 +0200

    Task 1.11* done

--- osa_1/unicafe/src/App.js
+++ osa_1/unicafe/src/App.js
@@ -10,9 +10,9 @@ const Button = (props) => {
 
 const StatisticLine = (props) => {
   return (
-    <div>
-      <b>{props.text} {props.value} {props.unit}</b>
-    </div>
+    <tr>
+      <td>{props.text} </td> <td>{props.value} {props.unit}</td>
+    </tr>
   )
 }
 
@@ -36,12 +36,14 @@ const Statistics = (props) => {
   }
   return (
     <div>
+      <table>
       <StatisticLine text="good" value={good} />
       <StatisticLine text="neutral" value={neutral} />
       <StatisticLine text="bad" value={bad} />
       <StatisticLine text="all" value={total} />
       <StatisticLine text="average" value={average} />
       <StatisticLine text="positive" value={positive} unit="%"/>
+      </table>
     </div>
   )
 
