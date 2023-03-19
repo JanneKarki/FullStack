0ddb73bf4782eed63987eec05e14013842e60f5a
Janne Kärki <janne.karki@helsinki.fi>
2023-03-19 15:22:43 +0200

    Task 4.1 done

---
+++ osa_4/bloglist/index.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const Blog = require('./models/blogs')
+
+
+
+const url = config.MONGODB_URI
+mongoose.connect(url)
+.then(() => {
+  console.log('connected to MongoDB')
+})
+.catch((error) => {
+  console.log('error connecting to MongoDB:', error.message)
+})
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+console.log('hello world')
+const PORT = process.env.PORT  || 3003
+app.listen(PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
+})
