5088d9775278e0a87104c54de22dd7a900dbbf55
Janne Kärki <janne.karki@helsinki.fi>
2023-03-25 21:01:58 +0200

    Corrected task 4.15 to use bcrypt

--- osa_4/bloglist/controllers/users.js
+++ osa_4/bloglist/controllers/users.js
@@ -1,6 +1,7 @@
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 require('express-async-errors')
+const bcrypt = require('bcrypt')
 
 usersRouter.get('/', async (request, response) => {
     const users = await User.find({})
@@ -23,11 +24,14 @@ usersRouter.get('/:id', (request, response, next) => {
 */
 usersRouter.post('/', async (request, response) => {
   const body = request.body
-    
+
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(body.password, saltRounds)
+
   const user = new User({
     username: body.username,
     name: body.name,
-    passwordHash: body.passwordHash,
+    passwordHash,
     //notes: body.notes
   })
 
